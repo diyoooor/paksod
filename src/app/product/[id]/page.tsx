fb07eb71303ab68691f8fe0433a62a6a752ecdc4
Mona Lisa <chutiphong.charoenchao@gmail.com>
2024-11-18 16:00:56 +0700

    ⚡️Implement :: Implement Product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface IProductDetail {
   params: Promise<{ id: string }>;
 }
@@ -5,5 +7,73 @@ interface IProductDetail {
 export default async function ProductDetail({ params }: IProductDetail) {
   const detail = decodeURIComponent((await params).id);
 
-  return <div>Product Detail by ID: {detail}</div>;
+  return (
+    <div className="w-full">
+      <section>สินค้าทั้งหมด / ผักสด / มะเขือเทศ</section>
+      <section className="mt-10 w-full">
+        <Image
+          src={"/products/tomato.png"}
+          width={250}
+          height={250}
+          alt="product-name"
+          className=" mx-auto border w-full"
+        />
+      </section>
+      <section>
+        <h1 className="text-center text-3xl font-semibold">มะเขือเทศ</h1>
+        <ul className={`w-full grid grid-cols-3 text-center mt-6 gap-5`}>
+          {[1, 2, 3].map((item, index) => {
+            return (
+              <li key={index}>
+                <div className="border  rounded-xl py-2 bg-green-medium font-bold">
+                  หมวดหมู่ {item}
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+      <section className="mt-4">
+        <h1 className="text-start text-2xl font-semibold">รายละเอียดสินค้า</h1>
+        <p className=" indent-4">มะเขือเทศท้อ ราคาประหยัด 1 กก. 100 บาท</p>
+        <p className=" indent-4">มะเขือเทศท้อ ราคาประหยัด 10 กก. 1000 บาท</p>
+      </section>
+      <section className="mt-4">
+        <button className="w-full h-10 bg-green-dark text-white font-semibold rounded-xl">
+          สั่งซื้อ
+        </button>
+      </section>
+      <section className="mt-4">
+        <h1 className="text-start text-2xl font-semibold">
+          สินค้าที่เกี่ยวข้อง
+        </h1>
+        <ul className={`w-full flex flex-row overflow-auto gap-5 border`}>
+          {[1, 2, 3, 4, 5].map((item, index) => {
+            return (
+              <div key={index} className="bg-white rounded-xl min-w-48">
+                <Image
+                  alt={"highlight"}
+                  src={`/products/broccoli.png`}
+                  width={100}
+                  height={90}
+                  className="object-contain mx-auto py-10 drop-shadow-sm h-48 w-fit"
+                />
+                <div className="p-2">
+                  <p className="text-xl font-semibold text-green-dark">
+                    สินค้าที่ {index}
+                  </p>
+                  <p>กิโลกรัมละ 15 </p>
+                </div>
+                <div className="p-2">
+                  <button className="w-full h-10 bg-green-dark text-white font-semibold rounded-xl">
+                    สั่งซื้อ
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </ul>
+      </section>
+    </div>
+  );
 }
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,15 +1,113 @@
+"use client";
+import {
+  IconCarrot,
+  IconFish,
+  IconMeat,
+  IconBottle,
+  IconEggs,
+  IconClockHour3,
+} from "@tabler/icons-react";
+import Image from "next/image";
 import React from "react";
 
+interface ICategories {
+  id: number;
+  name: string;
+  icon: React.ReactNode;
+}
+
 const ProductsPage = () => {
+  const categories: ICategories[] = [
+    {
+      id: 1,
+      name: "ผักสด",
+      icon: <IconCarrot className="h-9 w-9" stroke={1.5} />,
+    },
+    {
+      id: 2,
+      name: "อาหารทะเล",
+      icon: <IconFish className="h-9 w-9" stroke={1.5} />,
+    },
+    {
+      id: 3,
+      name: "เนื้อสัตว์",
+      icon: <IconMeat className="h-9 w-9" stroke={1.5} />,
+    },
+    {
+      id: 4,
+      name: "เครื่องปรุง",
+      icon: <IconBottle className="h-9 w-9" stroke={1.5} />,
+    },
+    {
+      id: 5,
+      name: "ไข่",
+      icon: <IconEggs className="h-9 w-9" stroke={1.5} />,
+    },
+    {
+      id: 6,
+      name: "ผักดอง",
+      icon: <IconClockHour3 className="h-9 w-9" stroke={1.5} />,
+    },
+  ];
   return (
-    <div>
-      ProductPage
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
+    <div className="w-full">
+      <section className="text-center py-4">
+        <p className="text-3xl font-bold">รายการสินค้า</p>
+      </section>
+      <section className="flex mt-4 flex-nowrap overflow-auto max-w-screen gap-4">
+        {categories.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="min-w-30 py-3 px-4 rounded-xl flex items-center gap-2 bg-white text-nowrap"
+            >
+              <p>{item.icon}</p>
+              <p className="text-xl">{item.name}</p>
+            </div>
+          );
+        })}
+      </section>
+      <section className="mt-4 flex justify-between  py-2 text-lg ">
+        <div>
+          <p>สินค้าทั้งหมด {"10"} รายการ</p>
+        </div>
+        <div>
+          <select className="rounded w-full bg-transparent text-right pr-2">
+            <option value="price-low-high">ราคา: ต่ำ - สูง</option>
+            <option value="price-high-low">ราคา: สูง - ต่ำ</option>
+            <option value="name-a-z">ชื่อ: ก - ฮ</option>
+            <option value="name-z-a">ชื่อ: ฮ - ก</option>
+          </select>
+        </div>
+      </section>
+      <section>
+        <div className="grid grid-cols-2 gap-4">
+          {[1, 2, 3, 4, 5, 6].map((item, index) => {
+            return (
+              <div key={index} className="bg-white rounded-xl ">
+                <Image
+                  alt={"highlight"}
+                  src={`/products/broccoli.png`}
+                  width={100}
+                  height={90}
+                  className="object-contain mx-auto py-10 drop-shadow-sm h-48 w-fit"
+                />
+                <div className="p-2">
+                  <p className="text-xl font-semibold text-green-dark">
+                    สินค้าที่ {index}
+                  </p>
+                  <p>กิโลกรัมละ 15 </p>
+                </div>
+                <div className="p-2">
+                  <button className="w-full h-10 bg-green-dark text-white font-semibold rounded-xl">
+                    สั่งซื้อ
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
     </div>
   );
 };
