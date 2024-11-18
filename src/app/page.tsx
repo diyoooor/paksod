4d8f34c1f5abcddc31b5cf018e02920d6acfb488
Mona Lisa <chutiphong.charoenchao@gmail.com>
2024-11-18 10:44:29 +0700

    ⚡️ Implement :: Implement home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,165 @@
+"use client";
+import {
+  IconBottle,
+  IconCarrot,
+  IconClockHour3,
+  IconCurrencyBaht,
+  IconEggs,
+  IconFish,
+  IconMeat,
+  IconSalad,
+  IconSparkles,
+  IconTruckDelivery,
+} from "@tabler/icons-react";
+import Image from "next/image";
+
+interface ICategories {
+  id: number;
+  name: string;
+  icon: React.ReactNode;
+}
+
+interface IWhyUs {
+  icon: React.ReactNode;
+  topic: string;
+  description: string;
+}
+
 export default function Home() {
-  return <div></div>;
+  const categories: ICategories[] = [
+    {
+      id: 1,
+      name: "ผักสด",
+      icon: <IconCarrot className="h-9 w-9" stroke={1.5} />,
+    },
+    {
+      id: 2,
+      name: "อาหารทะเล",
+      icon: <IconFish className="h-9 w-9" stroke={1.5} />,
+    },
+    {
+      id: 3,
+      name: "เนื้อสัตว์",
+      icon: <IconMeat className="h-9 w-9" stroke={1.5} />,
+    },
+    {
+      id: 4,
+      name: "เครื่องปรุง",
+      icon: <IconBottle className="h-9 w-9" stroke={1.5} />,
+    },
+    {
+      id: 5,
+      name: "ไข่",
+      icon: <IconEggs className="h-9 w-9" stroke={1.5} />,
+    },
+    {
+      id: 6,
+      name: "ผักดอง",
+      icon: <IconClockHour3 className="h-9 w-9" stroke={1.5} />,
+    },
+  ];
+
+  const whyUs: IWhyUs[] = [
+    {
+      icon: <IconSalad className="h-32 w-32" stroke={1} />,
+      topic: "สดใหม่",
+      description:
+        "��ักสดมี��ท��ิ์อา��ารที่มีคุ����า��ที่ดีที่สุดสำหรับ��ู้สัตว์ และมีคุ����า��ในการรับประทานอา��ารต่อเนื่อง",
+    },
+    {
+      icon: <IconTruckDelivery className="h-32 w-32" stroke={1} />,
+      topic: "สะดวก รวดเร็ว",
+      description:
+        "อา��ารทะเลมีคุ����า��ที่ดีที่สุดสำหรับสัตว์ และมีคุ����า��ในการรับประทานอา��ารต่อเนื่อง",
+    },
+    {
+      icon: <IconCurrencyBaht className="h-32 w-32" stroke={1} />,
+      topic: "ราคาโดนใจ",
+      description:
+        "เนื้อสัตว์มีคุ��สมบัติที่ดีที่สุดสำหรับสัตว์ และมีคุ��สมบัติในการรับประทานอา��ารต่อเนื่อง",
+    },
+  ];
+
+  return (
+    <div>
+      <section className="inset-0 w-screen -mt-4 -mx-4 m-0 h-64 ">
+        <div>
+          <Image
+            alt={"banner"}
+            src={"/logo/banner.png"}
+            width={400}
+            height={190}
+            className=" object-contain"
+          />
+        </div>
+      </section>
+      <section className="flex mt-4 flex-nowrap overflow-auto max-w-screen gap-4">
+        {categories.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="min-w-30 py-3 px-4 rounded-xl flex items-center gap-2 bg-white text-nowrap"
+            >
+              <p>{item.icon}</p>
+              <p className="text-xl">{item.name}</p>
+            </div>
+          );
+        })}
+      </section>
+      <section className="mt-4 bg-red-400 rounded-xl p-4">
+        <h1 className="text-3xl py-4 font-semibold inline-flex gap-2">
+          <IconSparkles />
+          สินค้าแนะนำ
+        </h1>
+        <div className="grid grid-cols-2 gap-4">
+          {[1, 2, 3, 4, 5, 6].map((item, index) => {
+            return (
+              <div key={index} className="bg-white rounded-xl ">
+                <Image
+                  alt={"highlight"}
+                  src={`/products/broccoli.png`}
+                  width={100}
+                  height={90}
+                  className="object-contain mx-auto py-10 drop-shadow-sm h-48 w-fit"
+                />
+                <div className="p-2">
+                  <p className="text-xl font-semibold text-green-dark">
+                    สินค้าที่ {index}
+                  </p>
+                  <p>กิโลกรัมละ 15 </p>
+                </div>
+                <div className="p-2">
+                  <button className="w-full h-10 bg-green-dark text-white font-semibold rounded-xl">
+                    ใส่ตะกร้า
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="w-full mt-4 flex">
+          <button className="mx-auto rounded-lg bg-green-dark w-3/5">
+            <p className="text-lg font-semibold text-white p-2">ดูทั้งหมด</p>
+          </button>
+        </div>
+      </section>
+      <section className="mt-4 grid grid-flow-row gap-4 px-4">
+        {whyUs.map((item, index) => {
+          return (
+            <div key={index} className="text-center bg-white rounded-xl">
+              <div className="p-4 flex flex-col">
+                <p className="mx-auto">{item.icon}</p>
+                <h1 className="text-3xl font-semibold py-4">{item.topic}</h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                  consectetur, nisi sed viverra ultricies, nunc odio consectetur
+                  ligula, ac varius sem lectus ut velit.
+                </p>
+              </div>
+            </div>
+          );
+        })}
+      </section>
+    </div>
+  );
 }
--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -4,17 +4,44 @@ import { ILayout } from "./RootLayout";
 import Image from "next/image";
 import {
   IconBasket,
+  IconBrandFacebook,
+  IconBrandLine,
   IconHome,
+  IconPhone,
   IconSearch,
   IconUser,
 } from "@tabler/icons-react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+interface ISocial {
+  icon: React.ReactNode;
+  name: string;
+  link: string;
+}
 
 const MainLayout = ({ children }: ILayout) => {
+  const socials: ISocial[] = [
+    {
+      icon: <IconBrandFacebook stroke={1} />,
+      name: "Facebook",
+      link: "https://www.facebook.com/profile.php?id=61558650133542",
+    },
+    {
+      icon: <IconBrandLine stroke={1} />,
+      name: "Line",
+      link: "https://www.line.me/",
+    },
+    {
+      icon: <IconPhone stroke={1} />,
+      name: "Phone",
+      link: "tel:080-000-0000",
+    },
+  ];
   const router = useRouter();
   return (
     <div className="flex flex-col min-h-screen">
-      <header className=" text-white p-4 flex items-center justify-between h-20">
+      <header className=" text-white p-4 flex items-center justify-between h-20 sticky top-0 z-10 bg-white  drop-shadow-lg">
         <h1 className="text-lg font-semibold">
           <Image
             src={"/logo/logo.png"}
@@ -40,17 +67,35 @@ const MainLayout = ({ children }: ILayout) => {
           </ul>
         </nav>
       </header>
-      <main className="flex-1 bg-green-medium overflow-y-auto p-4">
+      <main className="flex-1 bg-gray-200 overflow-y-auto p-4 relative">
         {children}
       </main>
-      <section className="p-4">
+      <footer className="p-4 flex justify-around mx-auto gap-4 bg-green-dark w-full">
+        {socials.map((item, index) => {
+          return (
+            <Link
+              key={index}
+              href={item.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mx-auto text-green-light  text-center flex flex-col justify-center"
+            >
+              <p className="border border-green-dark self-center flex justify-center items-center w-10 h-10 rounded-lg">
+                {item.icon}
+              </p>
+              <p>{item.name}</p>
+            </Link>
+          );
+        })}
+      </footer>
+      <section className="p-2 sticky bottom-0 bg-white drop-shadow-md">
         <div className="flex gap-4 text-center p-4">
           <button className="mx-auto" onClick={() => router.push("/")}>
-            <IconHome className=" w-10 h-10 stroke-green-black " stroke={1} />
+            <IconHome className=" w-12 h-12 stroke-green-black " stroke={1} />
           </button>
           <button className="mx-auto" onClick={() => router.push("/search")}>
             <IconSearch
-              className="mx-auto w-10 h-10 stroke-green-dark"
+              className="mx-auto w-12 h-12 stroke-green-dark"
               stroke={1}
             />
           </button>
